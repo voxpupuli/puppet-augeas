2e9b9f7bd36895e9183795cc498b6cdcb0592466
Atha Kouroussis <athak@vurbia.com>
2010-12-22 22:56:33 -0300

    Refactored regexes for OS detection

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@ class augeas {
 	}
 
 	case $operatingsystem {
-		/RedHat|CentOS|Fedora/:   { include augeas::redhat }
-		/Debian|Ubuntu|kFreeBSD/: { include augeas::debian }
-		default:                  { include augeas::base }
+		/(?i)(RedHat|CentOS|Fedora)/:   { include augeas::redhat }
+		/(?i)(Debian|Ubuntu|kFreeBSD)/: { include augeas::debian }
+		default:                        { include augeas::base }
 	}
 }
