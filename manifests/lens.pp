77e920f2cd1ac21b9882d32d001b72b6765ae1ba
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-03-26 10:51:38 +0200

    Use subscribe instead of notify

--- manifests/lens.pp
+++ manifests/lens.pp
@@ -33,16 +33,12 @@ define augeas::lens (
   file { "${lens_dest}":
     ensure => $ensure,
     source => $lens_source,
-# We should actually run the test when the lens is changed
-# but we need to know if the test exists...
-#    notify => Exec["Typecheck lens ${name}", "Test lens ${name}"]
-    notify => Exec["Typecheck lens ${name}"]
   }
 
   exec { "Typecheck lens ${name}":
     command => "augparse -I ${augeas::base::lens_dir} ${lens_dest} || (rm -f ${lens_dest} && exit 1)",
-    require => File["${lens_dest}"],
     refreshonly => true,
+    subscribe => File[$lens_dest],
   }
 
   if $test_source {
@@ -54,8 +50,8 @@ define augeas::lens (
   
     exec { "Test lens ${name}":
       command => "augparse -I ${augeas::base::lens_dir} ${test_dest} || (rm -f ${lens_dest} && rm -f ${test_dest} && exit 1)",
-      require => File["${lens_dest}", "${test_dest}"],
       refreshonly => true,
+      subscribe => File[$lens_dest, $test_dest],
     }
   }
 }
