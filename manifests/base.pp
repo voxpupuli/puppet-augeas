a602fe6540fbf7b367752acfd32fcfc0803013c0
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-04-04 18:00:20 +0200

    Get the augeas module through puppet-lint

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,15 +1,15 @@
 class augeas::base {
-  $lens_dir = "/usr/share/augeas/lenses/contrib"
+  $lens_dir = '/usr/share/augeas/lenses/contrib'
 
   # ensure no file not managed by puppet ends up in there.
-  file { "${lens_dir}":
+  file { $lens_dir:
     ensure  => directory,
     recurse => true,
     purge   => true,
     force   => true,
-    mode    => 0644,
-    owner   => "root",
-    group   => "root",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
   }
 
   file { "${lens_dir}/tests":
@@ -17,8 +17,8 @@ class augeas::base {
     recurse => true,
     purge   => true,
     force   => true,
-    mode    => 0644,
-    owner   => "root",
-    group   => "root",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
   }
 }
