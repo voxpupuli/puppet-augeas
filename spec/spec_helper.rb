d51dc4b3501e60c4480659a234884a14eabd7696
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-03-15 20:49:03 +0100

    Simplify spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,33 +1,14 @@
-dir = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH.unshift File.join(dir, 'lib')
-
-# Don't want puppet getting the command line arguments for rake or autotest
-ARGV.clear
-
-require 'puppet'
-require 'facter'
 require 'mocha'
-gem 'rspec', '>=2.0.0'
-require 'rspec/expectations'
-
-require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
-RSpec.configure do |config|
-  # FIXME REVISIT - We may want to delegate to Facter like we do in
-  # Puppet::PuppetSpecInitializer.initialize_via_testhelper(config) because
-  # this behavior is a duplication of the spec_helper in Facter.
-  config.before :each do
-    # Ensure that we don't accidentally cache facts and environment between
-    # test cases.  This requires each example group to explicitly load the
-    # facts being exercised with something like
-    # Facter.collection.loader.load(:ipaddress)
-    Facter::Util::Loader.any_instance.stubs(:load_all)
-    Facter.clear
-    Facter.clear_messages
-  end
-  config.module_path = File.join(fixture_path, 'modules')
-  config.manifest_dir = File.join(fixture_path, 'manifests')
+RSpec.configure do |c|
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
+
+
+dir = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift File.join(dir, 'lib')
+
