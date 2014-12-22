520b5e876c5a31b9f4bf38a3714a125316833dd3
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-22 18:34:19 +0100

    Fix unit tests when using future parser

--- spec/hosts/with_lens_noaugeas_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'with_lens_noaugeas' do
-  it 'should error' do
-    expect {
-      is_expected.to contain_file('/usr/share/augeas/lenses/foo.aug')
-    }.to raise_error(Puppet::Error, /You must declare the augeas class/)
-  end
-end
