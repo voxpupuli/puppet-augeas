969a8fbb64ff5a001943376ae5e40bdf2e92f1ce
Joshua Hoblitt <josh@hoblitt.com>
2024-09-10 10:23:07 -0700

    drop Puppet Enterprise (pe) support
    As the pe detection logic was questionable, it has been removed.

--- functions/lens_dir.pp
+++ functions/lens_dir.pp
@@ -4,10 +4,8 @@
 function augeas::lens_dir() >> String {
   if $augeas::lens_dir {
     $augeas::lens_dir
-  } elsif versioncmp($facts['puppetversion'], '4.0.0') >= 0 and 'aio_agent_version' in $facts {
+  } elsif 'aio_agent_version' in $facts {
     $augeas::aio_lens_dir
-  } elsif 'is_pe' in $facts and $facts['is_pe'] {
-    $augeas::pe_lens_dir
   } else {
     $augeas::system_lens_dir
   }
